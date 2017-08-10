bbd7948292cc750f6976f4521f5eaef28807d5a1
Dylan McDonald <dmcdonald21@cnm.edu>
2017-08-09 19:04:16 -0600

    routed politician component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {PoliticianComponent} from "./components/politician.component";
 
 
-export const allAppComponents = [ HomeComponent];
+export const allAppComponents = [HomeComponent, PoliticianComponent];
 
 export const routes: Routes = [
+	{path: "politician", component: PoliticianComponent},
 	{path: "", component: HomeComponent}
 ];
 
